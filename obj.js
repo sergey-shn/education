654c1e4dbc601e239ddfef75854ccb8f0f0b22cf
Sergey <cergey141516@gmail.com>
2022-08-30 20:57:07 +0900

    Контекст + пример с массивами

--- obj.js
+++ obj.js
@@ -48,7 +48,6 @@
 // div.innerHTML = "<strong>ПРивет</strong>";
 // document.body.append(div);
 
-
 // li.before("before");
 // li.after("after");
 // let olList = document.createElement("ol");
@@ -78,5 +77,49 @@ function hello() {
 const person = {
   name: "Svatlana",
   age: 25,
-  s: hello
-}
+  male: "female",
+  sayHello: hello,
+  sayHelloWindow: hello.bind(document),
+  logInfo: function (job, phone) {
+    console.group(`${this.name} info:`);
+    console.log(`Name is ${this.name}`);
+    console.log(`Age is ${this.age}`);
+    console.log(`Male is ${this.male}`);
+    console.log(`Job is ${job}`);
+    console.log(`Phone is ${phone}`);
+    console.groupEnd();
+  },
+};
+
+const gena = {
+  name: "Gena",
+  age: 23,
+  male: "male",
+};
+
+// person.logInfo.bind(gena, 'Frontend', '8-999-293-22-33')()  // Bind позволяет задавать контекст и возвращает функцию, функцию можно вызвать в любое время.
+
+person.logInfo.call(gena, "Frontend", "8-999-293-22-33"); // Call позволяет задвать контекст, но вызывает функцию сразу.
+
+person.logInfo.apply(gena, ["Frontend", "8-999-293-22-33"]); // Apply то же самое что и call, но принимает только 2 аргумента и 2 аргумент всегда массив.
+// =================================
+
+const array = [2, 8, 4, 4, 5, 7];
+
+
+const array2 = [2, 4, 5, 6, 7, 1, 2];
+// function sortt(arr, n) {
+//   return array.map(function(i){
+//     return i * n;
+//   })
+// }
+
+Array.prototype.Sorrt = function (n) {
+  return this.map(function (i) {
+    return i * n;
+  });
+};
+
+
+console.log(array.Sorrt(28));
+console.log(array2.Sorrt(100));
